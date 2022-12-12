c7a0bb241bb7bf9f1752b76426fa1a11f64987e2
solanokruger <solanokruger@gmail.com>
2022-12-12 16:30:36 -0300

    Projeto inicial

---
+++ usf/src/main/java/uol/compass/project/usf/UsfApplication.java
@@ -0,0 +1,13 @@
+package uol.compass.project.usf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsfApplication.class, args);
+	}
+
+}
---
+++ usf/src/test/java/uol/compass/project/usf/UsfApplicationTests.java
@@ -0,0 +1,13 @@
+package uol.compass.project.usf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
