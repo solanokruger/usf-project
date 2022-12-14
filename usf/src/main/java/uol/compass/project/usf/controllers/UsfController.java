f90c22eba61542a858e0d88d943a21979d4424a2
Rafael <rafinhalq@gmail.com>
2022-12-14 19:47:00 -0300

    Controller do find all

--- usf/src/main/java/uol/compass/project/usf/controllers/UsfController.java
+++ usf/src/main/java/uol/compass/project/usf/controllers/UsfController.java
@@ -2,8 +2,10 @@ package uol.compass.project.usf.controllers;
 
 import javax.validation.Valid;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 import uol.compass.project.usf.dto.request.UsfRequestDTO;
 import uol.compass.project.usf.dto.response.UsfResponseDTO;
+import uol.compass.project.usf.dto.response.UsfResponseParameters;
 import uol.compass.project.usf.repositories.UsfRepository;
 import uol.compass.project.usf.services.UsfServiceImpl;
 
@@ -26,6 +29,12 @@ public class UsfController {
     public ResponseEntity<UsfResponseDTO> create(@RequestBody @Valid UsfRequestDTO request) {
         UsfResponseDTO response = usfService.create(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<UsfResponseParameters> findAll(Pageable pageable) {
+        UsfResponseParameters response = usfService.findAll(pageable);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
 }
