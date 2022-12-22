7fa1e6663a9503a5e979a49e70880d3fc0ee2261
solanokruger <solanokruger@gmail.com>
2022-12-22 14:57:05 -0300

    fixed: merge conflicts

--- usf/src/main/java/uol/compass/project/usf/constants/ErrorCode.java
+++ usf/src/main/java/uol/compass/project/usf/constants/ErrorCode.java
@@ -10,9 +10,9 @@ public enum ErrorCode {
     BAD_REQUEST("Pedido inválido"),
     INVALID_PARAMETER("Paramêtro do pedido inválido"),
     INTERNAL_SERVER_ERROR("Erro interno do servidor"),
-    USF_NOT_FOUND("USF não encontrada"),
-    TEAM_NOT_FOUND("Time não encontrado"),
-    DOCTOR_NOT_FOUND("Médico não encontrado"),
+//    USF_NOT_FOUND("USF não encontrada"),
+//    TEAM_NOT_FOUND("Time não encontrado"),
+//    DOCTOR_NOT_FOUND("Médico não encontrado"),
     SOLICITATION_NOT_FOUND("Pedido não encontrado");
 
     private final String message;
