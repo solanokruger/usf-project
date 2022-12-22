f99793723f2b0bc71f55c8c30701402722aab185
Rafael <rafinhalq@gmail.com>
2022-12-22 13:31:12 -0300

    Retorno da data em formato brasileiro

--- usf/src/main/java/uol/compass/project/usf/dto/response/SolicitationResponseDTO.java
+++ usf/src/main/java/uol/compass/project/usf/dto/response/SolicitationResponseDTO.java
@@ -2,6 +2,8 @@ package uol.compass.project.usf.dto.response;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,8 +19,10 @@ public class SolicitationResponseDTO {
 
     private Long idResource;
 
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime requestedDate;
 
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime answeredDate;
 
     private EnumStatusSolicitation statusSolicitation;
