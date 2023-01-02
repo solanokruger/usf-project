61b688c36be600a6c157e6a44fd3d317c5061eb1
Rafael <rafinhalq@gmail.com>
2023-01-02 18:07:36 -0300

    Retirando imports desnecessarios

--- usf/src/main/java/uol/compass/project/usf/config/security/role/RoleUserService.java
+++ usf/src/main/java/uol/compass/project/usf/config/security/role/RoleUserService.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
