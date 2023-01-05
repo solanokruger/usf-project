0162b3cc1427e509b6c3be46de9995774d8da7a5
Rafael <rafinhalq@gmail.com>
2023-01-05 14:33:32 -0300

    Imports

--- usf/src/main/java/uol/compass/project/usf/repositories/DoctorRepository.java
+++ usf/src/main/java/uol/compass/project/usf/repositories/DoctorRepository.java
@@ -1,5 +1,7 @@
 package uol.compass.project.usf.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import uol.compass.project.usf.model.entities.DoctorEntity;
@@ -7,5 +9,5 @@ import uol.compass.project.usf.model.entities.DoctorEntity;
 @Repository
 public interface DoctorRepository extends JpaRepository<DoctorEntity, Long> {
 
-    Page <DoctorEntity> findallByName (String name, Pageable pageable);
+    Page<DoctorEntity> findallByName (String name, Pageable pageable);
 }
